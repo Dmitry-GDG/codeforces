730e6fcd15988be902a9e98f434795465d05752b
dmitry gatilov <dmigatilov@gmail.com>
2023-08-18 16:10:20 +0300

    possible-friends slow, done but test 14 not ok

--- route256/possible-friends/main.go
+++ route256/possible-friends/main.go
@@ -31,9 +31,9 @@ func notInSlice(n int, ss []int) bool {
 	return true
 }
  
-func choseAndPrint(friends [][]int, id int) {
+func choseAndPrint(friends [][]int, id int, outp *[]int) {
 	res := make(map[int]int)
-	var outp []int
+	// var outp []int
 	var max int
 	
 	// соберём для конкретного пользователя карту пар потенциальных друзей: номер потенциального друга - количество общих друзей
@@ -45,10 +45,10 @@ func choseAndPrint(friends [][]int, id int) {
 				// выберем кандидатов с максимальным кол-вом общих друзей
 				switch {
 				case res[f] == max:
-					outp = append(outp, f)
+					*outp = append(*outp, f)
 				case res[f] > max:
 					max = res[f]
-					outp = append(outp[:0], f)
+					*outp = append((*outp)[:0], f)
 				}
 			}
 		}
@@ -58,22 +58,12 @@ func choseAndPrint(friends [][]int, id int) {
 	// выведем на печать максимально рекомендованных друзей
 	// при этом отсортируем список и восстановим нумерацию с 1
 	// outp = sort(outp)
-	sort.SliceStable(outp, func(i, j int) bool {
-		return outp[i] < outp[j]
+	sort.SliceStable(*outp, func(i, j int) bool {
+		return (*outp)[i] < (*outp)[j]
 	})
-	if len(outp) == 0 {
-		fmt.Println(0)
-		return
-	}
-	for i := 0; i < len(outp); i++ {
-		fmt.Print(outp[i] + 1) // восстановим нумерацию с 1
-		if i < len(outp) - 1 {
-			fmt.Print(" ")
-		}
-	}
-	fmt.Println()
+	// return outp
 }
- 
+
 func main() {
 	in := bufio.NewReader(os.Stdin)
 	out := bufio.NewWriter(os.Stdout)
@@ -82,7 +72,7 @@ func main() {
 	var n, m, k1, k2 int // n - количество пользователей, m - количество пар друзей
 	var inputData []Pair // срез входных данных - пар друзей
 	var friends [][]int // срез друзей каждого пользователя (0-первый пользователь, 1-2й пользователь и тд)
- 
+	
 	// распарсим  в срез
 	// при этом исправим нумерацию, начнём её с 0
 	fmt.Fscan(in, &n, &m)
@@ -93,27 +83,43 @@ func main() {
 	// fmt.Println("-- входные данные --\n", inputData, "\n-------------")
  
 	// соберём срезы друзей по каждому пользователю
+	var outp []int
 	for i := 0; i < n; i++ {
 		// var tmp []int
 		tmp := make([]int, 0, 5)
 		for _, id := range inputData {
 			if id.a1 == i {
 				tmp = append(tmp, id.a2)
-			} else if id.a2 == i {
-				tmp = append(tmp, id.a1)
+				} else if id.a2 == i {
+					tmp = append(tmp, id.a1)
+				}
 			}
-		}
 		// sort.SliceStable(tmp, func(i, j int) bool {
-		// 	return tmp[i] < tmp[j]
-		// })
-		// friends = append(friends, sort(tmp))
-		friends = append(friends, tmp)
-	}
-	// fmt.Println("-- срезы друзей по каждому пользователю --\n",friends, "\n-------------")
+			// 	return tmp[i] < tmp[j]
+			// })
+			// friends = append(friends, sort(tmp))
+			friends = append(friends, tmp)
+		}
+		// fmt.Println("-- срезы друзей по каждому пользователю --\n",friends, "\n-------------")
  
-	//поиск потенциальных друзей
-	for i := 0; i < n; i++ {
-		choseAndPrint(friends, i)
+		//поиск потенциальных друзей
+		for i := 0; i < n; i++ {
+		choseAndPrint(friends, i, &outp)
+		if len(outp) == 0 {
+			fmt.Fprintln(out, 0)
+			// fmt.Println(0)
+			// return
+		} else {
+			for i := 0; i < len(outp); i++ {
+				fmt.Fprint(out, outp[i] + 1) // восстановим нумерацию с 1
+				// fmt.Print(outp[i] + 1) // восстановим нумерацию с 1
+				if i < len(outp) - 1 {
+					fmt.Fprint(out, " ")
+				}
+			}
+			fmt.Fprintln(out)
+			outp = outp[:0]
+		}
 	}
 }
 
