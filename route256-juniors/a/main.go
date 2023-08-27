6a273e58120d2ebb5e642a0577b48547d4ff4b63
dmitry gatilov <dmigatilov@gmail.com>
2023-08-27 19:16:23 +0300

    a

--- route256-juniors/a/main.go
+++ route256-juniors/a/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"bufio"
-	"os"
+	// "bufio"
+	// "os"
 )
 
 func main() {
-	in := bufio.NewReader(os.Stdin)
-	out := bufio.NewWriter(os.Stdout)
-	defer out.Flush()
-	
+	// in := bufio.NewReader(os.Stdin)
+	// out := bufio.NewWriter(os.Stdout)
+	// defer out.Flush()
+	fmt.Println("I am sure that I will fill out the form by 23:59 on August 27, 2023.")
 }
