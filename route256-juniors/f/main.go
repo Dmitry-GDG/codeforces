51e569d48af5c37f67127ab38f5b0ec81fdabc77
dmitry gatilov <dmigatilov@gmail.com>
2023-08-27 19:25:40 +0300

    f

--- route256-juniors/f/main.go
+++ route256-juniors/f/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"bufio"
 	"os"
+	"encoding/json"
+	// "bytes"
 )
 
 func main() {
@@ -11,4 +13,30 @@ func main() {
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
 	
+	var t, n int
+	
+	
+	fmt.Fscan(in, &t)
+	var s, sCombo string
+	for ; t > 0; t-- {
+		fmt.Fscan(in, &n)
+		for ; n > 0; n-- {
+			fmt.Fscan(in, &s)
+			sCombo += s
+		}
+		if t > 1 {
+			sCombo += ", "
+		}
+	}
+	data, _ := json.Marshal(sCombo)
+	// data = bytes.Trim(data, "'\'")
+	for i := 0; i < len(data); i++ {
+		if i < len(data) - 1 && data[i] == 92 && data[i+1] == 34 {
+			// i++
+			continue
+		}
+		fmt.Printf("%s" , string(data[i]))
+	}
+	fmt.Println()
+	// fmt.Printf("%s\n", data)
 }
