eeffe35683d7ad247a189bdd37f2faf6c9dbb35e
dmitry gatilov <dmigatilov@gmail.com>
2023-08-27 19:19:12 +0300

    c

--- route256-juniors/c/main.go
+++ route256-juniors/c/main.go
@@ -11,4 +11,32 @@ func main() {
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
 	
+	var t, n, ai, max, min int
+	var sign string
+
+	fmt.Fscan(in, &t)
+
+	for i := 0; i < t; i++ {
+		min, max = 15, 30
+		fmt.Fscan(in, &n)
+		for j := 0; j < n; j++ {
+			fmt.Fscan(in, &sign, &ai)
+			if min == -1 {
+				fmt.Println("-1")
+				continue
+			}
+			if sign == "<=" && ai < max {
+				max = ai
+			} else if sign == ">=" && ai > min {
+				min = ai
+			}
+			if max < min {
+				min = -1
+			}
+			fmt.Println(min)
+		}
+		if i < t - 1 {
+			fmt.Println()
+		}
+	}
 }
