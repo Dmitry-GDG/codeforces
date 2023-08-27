04d8cd6200139a70208118fe0c00aa1db30c4b1c
dmitry gatilov <dmigatilov@gmail.com>
2023-08-27 19:21:16 +0300

    d

--- route256-juniors/d/main.go
+++ route256-juniors/d/main.go
@@ -11,4 +11,35 @@ func main() {
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
 	
+	var t, k, n, m int
+	fmt.Fscan(in, &t)
+
+	for ; t > 0; t-- {
+		var s string
+		var outp [][]byte
+		fmt.Fscan(in, &k, &n, &m)
+		for j := 0; j < n; j ++ {
+			fmt.Fscan(in, &s)
+			outp = append(outp, []byte(s))
+		}
+		for i := 1; i < k; i++ {
+			for j := 0; j < n; j ++ {
+				fmt.Fscan(in, &s)
+				for h := 0; h < m; h++ {
+					if outp[j][h] == '.' && s[h] != '.' {
+						outp[j][h] = s[h]
+					}
+				}
+			}
+		}
+		for i := 0; i < n; i++ {
+			for j := 0; j < m; j++ {
+				fmt.Print(string(outp[i][j]))
+			}
+			fmt.Println()
+		}
+		if t > 1 {
+			fmt.Println()
+		}
+	}
 }
